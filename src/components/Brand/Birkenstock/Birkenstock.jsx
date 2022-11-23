5cb59357725b652ca9cdc54dcf737deca81de9f4
Nurbaeva Assol <nurbaevaassol@gmail>
2022-11-23 19:12:36 +0300

    brend

---
+++ src/components/Brand/Birkenstock/Birkenstock.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Birkenstock = () => {
+  return (
+    <div className='birkenstock'>
+        <div className="container">
+            <div className="birkenstock__blook">
+            <h3 className='birkenstock__h3'>Бренд</h3>
+                <h2 className='birkenstock__title'>birkenstock</h2>
+                <p className='birkenstock__p'>BIRKENSTOCK — известная немецкая компания, занимающаяся производством ортопедической обуви. </p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Birkenstock
---
+++ src/components/Brand/Brand.jsx
@@ -0,0 +1,19 @@
+
+import React from 'react'
+
+const Brand = () => {
+  return (
+    <div className='brand' >
+        <div className="container">
+            <div className="brand__title">
+                <h3 className='brand__h3'>Бренд</h3>
+                <h2 className='brand__h2'>Deha</h2>
+                <p className='brand__p'>Бренд DEHA принадлежит компании Meeting Group SpA,
+                     которая является лидером на итальянском рынке спортивной одежды премиум-класса. </p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Brand
